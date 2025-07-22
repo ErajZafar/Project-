06b54e5d2eca1bc69174ce094fbad2fbbb6cb9f4
AREEJ <13122980+ErajZafar@users.noreply.github.com>
2025-07-22 19:30:51 +0500

    Add files via upload

---
+++ info = {.swift
@@ -0,0 +1,15 @@
+info = {
+    "key" : "value",
+    "name" : "apnacollege",
+    "learing":"coding",
+    "age" : "33",
+    "is_adult" : "True",
+    "marks" : "94.9",
+
+}
+
+print (info);
+
+
+
+
---
+++ lecture 4.py
@@ -0,0 +1,13 @@
+student = {
+    "name" : "rahul kumar",
+    "students" : {
+        "python" : 98,
+        "Arti.I" : 86,
+        "electronics" : 96
+    }
+}
+
+student.update({"city":"London","age":23})
+student.update(new_dict)
+new_dict
+print(student)
