b0eb13028cf43b90b5a9aea09114c81f4123c15d
AREEJ <13122980+ErajZafar@users.noreply.github.com>
2025-07-24 17:15:25 +0500

    Add files via upload

---
+++ #lecture_four.py
@@ -0,0 +1,13 @@
+#lecture_four.py tuples
+student = {
+    "name" : "rahul kumar",
+    "students": {
+      "Electronics": 70,
+      "computer science" : 72,
+      "German" : 65
+     }
+}
+new_dict = {"city":"delhi","age" : "12"} 
+student.update(new_dict)    
+new_dict.fromkeys
+print(student)
