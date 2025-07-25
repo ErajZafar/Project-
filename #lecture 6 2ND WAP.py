7967a222aeaab883bf0886c397e9c267702f9dca
AREEJ <13122980+ErajZafar@users.noreply.github.com>
2025-07-25 19:47:23 +0500

    lecture _06 Functions

---
+++ #lecture 6 2ND WAP.py
@@ -0,0 +1,16 @@
+#lecture 6.py_ 2nd WAP
+cities = ["Karachi","luton","watford","hatfield","St.Albans","Coventry"]
+heroes = ["Diana","wonderwoman","Robots","ironwoman","Beastwoman"]
+
+print(heroes[0], end=" ")  # \n can be added as equal to  end
+print(heroes[1], end=" ")  # \n can be added as equal to  end
+
+def print_len(list):
+    print(len(list))
+
+def print_len(list):
+    for item in list:
+        print(item, end=" ")
+
+print_len(cities) #print()  when you want to remove % from the termial out portion 
+print()
---
+++ #lecture_6 WAP.py
@@ -0,0 +1,9 @@
+
+cities = ["Karachi","luton","watford","hatfield","St.Albans","Coventry"]
+heroes = ["Diana","wonderwoman","Robots","ironwoman","Beastwoman"]
+
+def print_len(list):   #for length of cites defining a function
+    print(len(list))   # lists can be replaced by cities here vice versa
+
+    print_len(cities)
+    print_len(heroes)
